7bfc0ee8f48e753663de80c659d68d64f3e0286a
Rex Li <rli233@MacBook-Pro.local>
2019-09-03 15:27:10 -0400

    updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const fs = require('fs');
 const express = require('express')
+var Chess = require('chess.js').Chess;
 const app = express()
 const port = 3000;
 app.set('view engine', 'ejs');
@@ -8,6 +9,11 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('pages/index');
+});
+
+app.get('/game', function(req, res) {
+    loadOneGame()
+    res.status(200).json({message: "creating fen table"});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
@@ -19,14 +25,36 @@ const lichessApi = 'https://lichess.org/api'
 console.log(__filename)
 console.log(__dirname)
 
-request.get(lichessApi+'/account', function(err, res, body){ 
-    obj = JSON.parse(body)
-    console.log(obj.id + "\nclassical:"+obj.perfs.classical.rating + "\trapid:"+obj.perfs.rapid.rating)
-}).auth(null, null, true, personalToken);
+function getAccountDetails(){
+    request.get(lichessApi+'/account', function(err, res, body){ 
+        obj = JSON.parse(body)
+        console.log(obj.id + "\nclassical:"+obj.perfs.classical.rating + "\trapid:"+obj.perfs.rapid.rating)
+    }).auth(null, null, true, personalToken);
+}
 
 
 function getAllMyGames(){
     request.get(lichessApi+'/games/user/a12233', function(err, res){
         console.log(res.body)
     }).auth(null, null, true, personalToken).pipe(fs.createWriteStream(__dirname+'/gameData/game2.pgn'));
+}
+
+function loadOneGame(){
+    fs.readFile('gameData/game.pgn', (err, data) => {
+        if (err) throw err;
+        // console.log(data.toString());
+        var chess = new Chess(); 
+        chess.load_pgn(data.toString());
+        moves = chess.history()
+        console.log(moves)
+        var chess1 = new Chess();
+        var stream = fs.createWriteStream("gameFen.txt", {flags:'a'});
+        var iter = 0 
+        for(var iter = 0; iter< moves.length; iter++){
+            chess1.move(moves[iter]);
+            fen = chess1.fen()
+            stream.write(fen+"\n")
+        }
+        console.log(chess.fen()) //ending position 
+      });
 }
