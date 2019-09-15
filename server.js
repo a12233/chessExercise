164f297803d2df123e21317851152fe77ea48e63
Rex Li <rli233@MacBook-Pro.local>
2019-09-14 21:36:55 -0400

    /db route

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/db', async (req, res) => {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM mygames');
       const results = { 'results': (result) ? result.rows : null};
-      response.status(200).json(results.rows)
+      res.status(200).json(results.rows)
     client.release();
     } catch (err) {
       console.error(err);
