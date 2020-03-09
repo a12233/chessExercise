6db7c42027354b25ccd392e9f65730d92dbb4eb6
Rex Li <rli233@MacBook-Pro.local>
2020-03-09 14:41:09 -0400

    refactoring

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express()
 const port = 3000;
 app.set('view engine', 'ejs');
 const jsdom = require('jsdom')
+var Worker = require("tiny-worker");
+var worker = new Worker("stockfish.js");
 // const { Worker } = require('worker_threads');
 // const { workerData, parentPort } = require('worker_threads')
 
@@ -41,7 +43,7 @@ const { Pool } = require('pg');
 let connectionString = {};
 if (env === 'local') {
     connectionString = {
-        user: 'rex',
+        user: 'postgres',
         host: 'localhost',
         database: 'chess',
         password: 'admin',
@@ -112,7 +114,13 @@ app.get('/', async function(req, res) {
     // pouch.sync()
     // createFen("e4 e5 Nf3 Nc6 Bb5 Nd4 Bc4 Nxf3+ Qxf3 Nf6 Nc3 c6 d3 h6 Be3 Bd6 O-O-O O-O h4 a5 g4 b5 g5 Nh7 gxh6 g6 h5 bxc4 hxg6 fxg6 Qg4 Qf6 Rdg1 Be7 Qxg6+ Qxg6 Rxg6+ Kh8 Rhg1 cxd3 Rg8+ Rxg8")
     // run().catch(err => console.error(err))
-    worker.postMessage("uciok");
+    // worker.postMessage("uci");
+    worker.postMessage('setoption name Ponder value false');
+    worker.postMessage('setoption name MultiPV value 3');
+
+    worker.postMessage('position fen r1b3rk/3pb2n/2p4P/p3p3/4P3/2NpB3/PPP2P2/2K3R1 w - - 0 22');
+    // worker.postMessage('go movetime ' + '1000');
+    
     res.render('pages/index');
 });
 
@@ -138,8 +146,7 @@ app.get('/viz', (req, res)=>{
 });
 
 app.get('/latestGame', async (req, res) => {
-    var id = 'qwKH00va'
-    id = await getLatestGame() 
+    let id = await getLatestGame() 
     const data = await pool.query('SELECT * FROM mygames WHERE id=$1', [id]); 
     return res.render('pages/latestGame', {
             "data":data.rows[0].moves,
@@ -248,16 +255,34 @@ function insertAllGames(id, moves, color, data ) {
 }
 //extract relevant data and write to another file, eventually write to DB 
 async function parsingAllGamesFiles(){
-    lr = new LineByLineReader('gameData/testGames.txt');
-    var data = fs.readFileSync('');
-    var jsonData = JSON.parse(data)
-    for(let i = 0; i < jsonData.length ; i++){
-        var obj = jsonData[i]
-        var color = ''
-        if(obj.players.white.user.id == 'a12233') color = 'white'
-        else color = 'black'
-        await insertAllGames(obj.id, obj.moves, color, obj)
-    }
+    lr = new LineByLineReader('gameData/feb2020.pgn');
+    lr.on('error', function (err) {
+       console.log(err)
+    });
+
+    lr.on('line', function (line) {
+        // pause emitting of lines...
+        lr.pause();
+
+        // ...do your asynchronous line processing..
+        setTimeout(async function () {
+            var jsonData = JSON.parse(line)
+            for(let i = 0; i < jsonData.length ; i++){
+                var obj = jsonData[i]
+                var color = ''
+                if(obj.players.white.user.id == 'a12233') color = 'white'
+                else color = 'black'
+                await insertAllGames(obj.id, obj.moves, color, obj)
+            }
+            // ...and continue emitting lines.
+            lr.resume();
+        }, 100);
+    });
+
+    lr.on('end', function () {
+        console.log("done uploading")
+    });
+
 }
 //split games by color 
 async function splitGames(){
@@ -342,18 +367,10 @@ function testFgets(){
     }
 }
 
-var Worker = require("tiny-worker");
-var worker = new Worker("stockfish.js");
- 
-worker.onmessage = function (ev) {
-    console.log(ev.data);
-    worker.terminate();
-};
- 
 
 function runService(workerData) {
   return new Promise((resolve, reject) => {
-    const worker = new Worker('./service.js', { workerData });
+    const worker = new Worker('./stockfish.js', { workerData });
     worker.on('message', resolve);
     worker.on('error', reject);
     worker.on('exit', (code) => {
@@ -368,3 +385,15 @@ async function run() {
   console.log(result);
 }
 
+worker.onmessage = function (event) {
+    if (event.data.search(/^bestmove/) !== -1) {
+        var move = event.data;
+        // console.log(move)
+    }
+    if (event.data.search(/^info/) !== -1) {
+        // var move = event.data;
+        // console.log(move)
+        //info depth 12 seldepth 22 multipv 1 score cp -832 nodes 241951 nps 240987 hashfull 113 tbhits 0 time 1004 pv g1g8 h8g8 c2d3 c8a6 c1d2 h7g5 c3a4 g8h7 a4c5 e7c5 e3c5 h7h6 c5b6 g5f3 d2e2
+    }
+    console.log(event.data);
+};
